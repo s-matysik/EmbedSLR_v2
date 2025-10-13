58e637bbd2307a16fe863abb122c57f4d95fc023
s-matysik <158293088+s-matysik@users.noreply.github.com>
2025-10-14 01:11:11 +0200

    Update __init__.py

--- embedslr/__init__.py
+++ embedslr/__init__.py
@@ -3,13 +3,16 @@ from .embeddings import get_embeddings, list_models
 from .similarity import rank_by_cosine
 from .bibliometrics import full_report, indicators
 from .colab_app import run as colab_run
+from .advanced_scoring import rank_with_advanced_scoring
+from .config import ScoringConfig, ColumnMap
 
 try:
     __version__ = _m.version(__name__)
 except _m.PackageNotFoundError:
-    __version__ = "0.5.0"
+    __version__ = "0.6.0"
 
 __all__ = [
     "get_embeddings", "list_models", "rank_by_cosine",
     "full_report", "indicators", "colab_run",
+    "rank_with_advanced_scoring", "ScoringConfig", "ColumnMap"
 ]
